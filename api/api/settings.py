63b0f334f8651c86fabd690c7bcfe86ea69239f4
samuel <samuelmedeiros2025@gmail.com>
2025-08-01 01:03:28 +0000

    corrigindo erros

--- api/api/settings.py
+++ api/api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'pedidos',
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CSRF_TRUSTED_ORIGINS=['https://localhost:8000']
--- api/pedidos/urls.py
+++ api/pedidos/urls.py
@@ -1,7 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('pedidos.urls')),
+    path('criar/', views.PedidoCreateView.as_view(), name='criar_pedido'),
 ]
